2ae8ee2c6739d01548408edc803dd71a83a370cd
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-23 10:00:23 -0400

    Update the Terraform configuration to use the Azure identity values

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -11,11 +11,15 @@ terraform {
     storage_account_name = "akin0098githubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
+    use_oidc             = true
   }
 }
+
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
+
 resource "azurerm_resource_group" "app_rg" {
   name     = "akin0098-a12-rg"
   location = "Canada Central"
