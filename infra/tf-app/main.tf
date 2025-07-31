28e739f46f1303a51365d1b936e44a50e20dd816
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-31 13:00:15 -0400

    changed name

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,23 +1,4 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "akin0098-githubactions-rg"
-    storage_account_name = "akin0098githubactionsss"
-    container_name       = "tfstate"
-    key                  = "prod.app.tfstate"
-    use_oidc             = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
+resource "azurerm_resource_group" "app_rg" {
+  name     = "akin0098-githubactions-rg"
+  location = "Canada Central"
 }
---
+++ infra/tf-app/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "akin0098-githubactions-rg"
+    storage_account_name = "akin0098githubactionsss"
+    container_name       = "tfstate"
+    key                  = "prod.app.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
