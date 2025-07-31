1c27369f7c10885f4d581c89fab178b5b8ae52fd
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-31 18:38:57 -0400

    removed pip

--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -3,8 +3,3 @@ output "app_subnet_id" {
   value       = azurerm_subnet.app_subnet.id
   description = "The ID of the application subnet"
 }
-
-output "app_public_ip_address" {
-  value       = azurerm_public_ip.app_ip.ip_address
-  description = "The static public IP address for the application"
-}
--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -36,19 +36,6 @@ resource "azurerm_subnet" "app_subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "app_ip" {
-  name                = "app-public-ip"
-  location            = azurerm_resource_group.app_rg.location
-  resource_group_name = azurerm_resource_group.app_rg.name
-
-  allocation_method   = "Static"
-  sku                 = "Standard"
-
-  tags = {
-    environment = "production"
-    project     = "cst8918-lab12"
-  }
-}
 
 provider "azurerm" {
   features {}
