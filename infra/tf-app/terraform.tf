a31085437e644f98616caca2e187ad9f32332191
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-31 18:35:15 -0400

    added pip

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -36,32 +36,18 @@ resource "azurerm_subnet" "app_subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "app_nsg" {
-  name                = "app-nsg"
+resource "azurerm_public_ip" "app_ip" {
+  name                = "app-public-ip"
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
 
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  allocation_method   = "Static"
+  sku                 = "Standard"
 
   tags = {
     environment = "production"
     project     = "cst8918-lab12"
   }
-}
-
-resource "azurerm_subnet_network_security_group_association" "app_subnet_nsg_assoc" {
-  subnet_id                 = azurerm_subnet.app_subnet.id
-  network_security_group_id = azurerm_network_security_group.app_nsg.id
 }
 
 provider "azurerm" {
