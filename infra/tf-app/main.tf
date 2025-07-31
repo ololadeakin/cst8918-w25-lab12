a8389ecc7f76d8bd64200b925b4881d0137406f2
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-31 18:46:05 -0400

    added vm resource

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "app_rg" {
   name     = "akin0098-a12-rg"
   location = "Canada Central"
 }
+
+resource "azurerm_public_ip" "app_ip" {
+  name                = "app-public-ip"
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
