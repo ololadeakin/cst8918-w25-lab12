0ac40483329a841b399f0f5db8d577d63bbc8665
Jonathan Martins <mart1449@algonquinlive.com>
2025-07-31 18:54:30 -0400

    added vnet and subnet resource

--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -3,13 +3,3 @@ output "app_subnet_id" {
   value       = azurerm_subnet.app_subnet.id
   description = "The ID of the application subnet"
 }
-
-output "app_vm_public_ip" {
-  value       = azurerm_public_ip.app_ip.ip_address
-  description = "The public IP of the VM"
-}
-
-output "app_vm_fqdn" {
-  value       = azurerm_public_ip.app_ip.fqdn
-  description = "The DNS name (if you set one) or blank"
-}
--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -36,57 +36,24 @@ resource "azurerm_subnet" "app_subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "app_nic" {
-  name                = "app-vm-nic"
+resource "azurerm_virtual_network" "app_vnet_secondary" {
+  name                = "app-vnet-secondary"
+  address_space       = ["10.2.0.0/16"]
   location            = azurerm_resource_group.app_rg.location
   resource_group_name = azurerm_resource_group.app_rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.app_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.app_ip.id
-  }
 
   tags = {
     environment = "production"
     project     = "cst8918-lab12"
+    role        = "secondary-network"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "app_vm" {
-  name                = "app-vm"
-  resource_group_name = azurerm_resource_group.app_rg.name
-  location            = azurerm_resource_group.app_rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.app_nic.id,
-  ]
-
-  # You can replace with your own key or use password_authentication = true
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  tags = {
-    environment = "production"
-    project     = "cst8918-lab12"
-  }
+resource "azurerm_subnet" "app_subnet_secondary_vnet" {
+  name                 = "app-subnet-secondary"
+  resource_group_name  = azurerm_resource_group.app_rg.name
+  virtual_network_name = azurerm_virtual_network.app_vnet_secondary.name
+  address_prefixes     = ["10.2.1.0/24"]
 }
 
 provider "azurerm" {
